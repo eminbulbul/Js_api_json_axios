250326b80ea7128054b03a7fa3a13fa0aaf732b6
m_emin <mehmeteminbul@gmail.com>
2022-03-12 19:12:59 +0300

    session ended

--- userList.js
+++ userList.js
@@ -1,11 +1,54 @@
 const tbody = document.getElementById("tbodyUserList");
 
 window.onload = () => {
+  alert(`Your apiKey is: "${DecryptStringAES(localStorage.getItem("apiKey"))}`);
   getApiUserList();
+  setInterval(getApiUserList, 5000);
 };
 
 const getApiUserList = async () => {
   showLoading();
-  //aixos
-  removeLoading();
+  //////axios
+  try {
+    // const responseData = await axios("https://reqres.in/api/users?page=1");
+    const responseData = await axios({
+      url: "https://reqres.in/api/users?page=1",
+      method: "get",
+      // data:apiKey
+    });
+    const { data: userListArray } = responseData.data;
+    console.log(userListArray);
+    if (userListArray.length === 0) {
+      alert("userlist not found");
+      removeLoading();
+    } else {
+      tbody.innerHTML = "";
+      userListArray.forEach((customer) => {
+        tbody.innerHTML += `
+            <tr>
+                <td>
+					${customer.id}
+                </td>
+                <td>
+					<img src="${customer.avatar}">
+                </td>
+                <td>
+                    ${customer.email}
+                </td>
+                <td>
+                    ${customer.first_name}
+                </td>
+                <td>
+                    ${customer.last_name}
+                </td>
+            </tr>`;
+      });
+      removeLoading();
+    }
+  } catch (error) {
+    alert(error);
+    //send errors to api and database (errorLogs table)
+    // postErrorLog("userList", "getApiUserList", error);
+    removeLoading();
+  }
 };
